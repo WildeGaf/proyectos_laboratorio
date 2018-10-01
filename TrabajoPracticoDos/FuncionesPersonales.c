5dc67d956ba108f1af35259fcc13bfb0bf83c78f
WildeGaf <40776504+WildeGaf@users.noreply.github.com>
2018-10-01 09:40:29 -0300

    Add files via upload

---
+++ TrabajoPracticoDos/FuncionesPersonales.c
@@ -0,0 +1,242 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FuncionesPersonales.h"
+
+
+/************ FUNCIONES CALCULADORA **************/
+
+/**
+* @brief Se imprime una lista de opciones, en la cual el usuario debe seleccionar una.
+* @return Devuelve un valor correspondido a la opcion seleccionada.
+*/
+
+int menu(){
+    int opcion;
+    printf("Menu de la calculadora\n");
+    printf("\n1- Ingrese 1 para realizar una suma \n");
+    printf("2- Ingrese 2 para realizar una resta \n");
+    printf("3- Ingrese 3 para realizar una division \n");
+    printf("4- Ingrese 4 para realizar una multiplicacion \n");
+    printf("5- Ingrese 5 para realizar el factorial \n");
+    printf("6- Ingrese 6 para realizar todas las operaciones \n");
+    printf("\nDigite la opcion elegida: ");
+    scanf("%d",&opcion);
+    return opcion;
+}
+
+/**
+* @brief Suma dos valores enteros.
+* @param num1 El primer valor, para realizar la operacion.
+* @param num2 El segundo valor, para realizar la operacion.
+* @return Devuelve el resultado de la suma de los operadores ingresados.
+*/
+int suma(int num1, int num2){
+    int suma;
+    suma = (num1+num2);
+    return suma;
+}
+
+/**
+* @brief Resta dos valores enteros.
+* @param num1 El primer valor, para realizar la operacion.
+* @param num2 El segundo valor, para realizar la operacion.
+* @return Devuelve el resultado de la resta de los operadores ingresados.
+*/
+int resta(int num1, int num2){
+    int resta;
+    resta = num1-num2;
+    return resta;
+}
+
+/**
+* @brief Multiplica dos valores enteros.
+* @param num1 El primer valor, para realizar la operacion.
+* @param num2 El segundo valor, para realizar la operacion.
+* @return Devuelve el resultado de la multiplicacion de los operadores ingresados.
+*/
+int multiplicacion(int num1,int num2){
+    int multi;
+    multi = num1*num2;
+    return multi;
+}
+
+/**
+* @brief Divide dos valores enteros y si no es posible da mensaje de error.
+* @param num1 El primer valor, para realizar la operacion.
+* @param num2 El segundo valor, para realizar la operacion el cual debe ser distinto de 0.
+* @return Devuelve el resultado de la division de los operadores ingresados.
+*/
+int division(int num1,int num2){
+    int div;
+    div = num1/num2;
+    return div;
+}
+
+/**
+* @brief Realiza el factorial de un numero ingresado, en caso de que sea un numero negativo, da un mensaje de error.
+* @param num El numero al cual se desea realizar el factorial.
+* @return Devuelve el resultado del factorial del numero ingresado.
+*/
+int factorial(int num){
+    int contador,acumuladorUno=0;
+    if (num<0){
+        return -1;
+    }
+    else{
+        if (num==0 || num==1){
+           return 1;
+           }
+        else{
+            for (contador=num-1;contador!=0;contador--){
+                acumuladorUno = num * contador + acumuladorUno;
+            }
+        }
+    return acumuladorUno;
+    }
+}
+
+
+/**************ENTRADA DE DATOS*****************/
+
+
+void getInt(int* puntero)
+{
+    int numero;
+    scanf("%d",&numero);
+    *puntero = numero;
+}
+
+void getFloat(float* puntero)
+{
+    float numero;
+    scanf ("%f",&numero);
+    *puntero = numero;
+}
+
+void getChar(char* mensaje)
+{
+    char letra;
+    fflush(stdin);
+    scanf("%c",&letra);
+    *mensaje = letra;
+
+}
+
+int getString(char* input){
+scanf("%s",input);
+return 0;
+}
+
+/**************VALIDACION DE DATOS*****************/
+
+int esNumerico(char str[]){
+    int i=0;
+    while(str[i] != '\0' ){
+        if(str[i]<'0' || str[i] > '9')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+int esSoloLetras(char str[]){
+    int i=0;
+    while(str[i] != '\0'){
+        if ((str[i] != ' ') &&
+            (str[i] < 'a' || str[i] > 'z') &&
+            (str[i] < 'A' || str[i] > 'Z')){
+            return 0;}
+        i++;
+    }
+    return 1;
+}
+
+int esAlfaNumerico(char str[]){
+    int i=0;
+     while(str[i] != '\0'){
+        if ((str[i] != ' ') &&
+            (str[i] < 'a' || str[i] > 'z') &&
+            (str[i] < 'A' || str[i] > 'Z') &&
+            (str[i] < '0' || str[i] > '9'))
+        return 0;
+    }
+    return 1;
+}
+
+int esTelefono(char str[])
+{
+    int i = 0;
+    int contadorGuiones = 0;
+    while (str[i] != '\0'){
+        if ((str[i] != ' ') && (str[i] != '-') &&
+            (str[i] < '0' || str[i] > '9')){
+            return 0;}
+        if (str[i] == '-'){
+            contadorGuiones++;
+        }
+        i++;
+    }
+    if (contadorGuiones==1){
+        return 1;
+    }
+    return 0;
+}
+
+int getSringLetras(char input[]){
+    char aux[256];
+    getString(aux);
+    if (esSoloLetras(aux)){
+        strcpy(input,aux);
+        return 1;
+    }
+    return 0;
+}
+
+
+/**************FUNCIONES AGENDA*****************/
+
+int initEmployees(Employee* lista, int len){
+int i, retorno;
+retorno = -1;
+    for (i=0;i<len;i++){
+        if (lista[i].isEmpty == 0){
+            retorno = i;
+            break;
+        }
+    }
+return retorno;
+}
+
+
+int addEmployee(Employee* lista, int len,int id,
+                char name[],char lastName[],
+                float salary, int sector)
+{
+    int r;
+    r = initEmployees(lista,len);
+    if (r != -1){
+        strcpy(lista[r].name,name);
+        strcpy(lista[r].lastName,lastName);
+        lista[r].salary = salary;
+        lista[r].sector = sector;
+        lista[r].id = id;
+        lista[r].isEmpty = 1;
+    }
+    return 0;
+}
+
+int findEmployeeById(Employee* lista, int len,int id)
+{
+    int i;
+    for (i=0;i<len;i++){
+        if(lista[i].id == id){
+            return i;
+        }
+    }
+ return -1;
+}
+
+
+
+
--- TrabajoPracticoDos/main.c
+++ TrabajoPracticoDos/main.c
@@ -1,73 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CANT 2
-
-int menu(){
-    int respuesta;
-    printf("---------------Menu de la Agenda----------------");
-    printf("\n1- Ingrese uno para dar de alta");
-    printf("\n2- Ingrese dos para dar de baja");
-    printf("\n3- Ingrese tres para modificar un valor");
-    printf("\n4- Ingrese cuatro para imprimir los datos ordenados");
-    printf("\n5- Ingrese cinco para salir");
-    printf("\n\nDigite la opcion elegida: ");
-    scanf("%d",&respuesta);
-    return respuesta;
-}
-
-struct ePersonas{
-    char nombre[31];
-    char apellido[31];
-    char dni[31];
-    int dia,mes,anio,dataEmpty;
-};
-
-int espacioLibre(){
-    struct ePersonas per[CANT];
-    int i, retorno;
-    for(i=0;i<CANT;i++){
-        if (per[i].dataEmpty == 1){
-            per[i].dataEmpty = 0;
-            retorno = i;
-            break;
-        }else{
-        retorno = -1;
-        }
-    }
-    return retorno;
-
-}
+#include <string.h>
+#include "FuncionesPersonales.h"
+#define CANT 3
 
 int main()
 {
-    struct ePersonas per[200];
-    int i,bandera,eleccion,salida = 0;
-    do{
-        for (i=0;i<CANT;i++){
-            per[i].dataEmpty = 1;
-        }
-        eleccion = menu();
+    Employee lista[CANT];
+
+    char auxiliarNombre[51];
+    char auxiliarApellido[51];
+    char auxiliarChar[51];
+    int empleadoIdEncontrado;
+    int auxId;
+    int auxSector;
+    float auxSalario;
+    int indiceLugarLibre;
+    int eleccion = 0;
+    int i,j;
+
+    for (i=0;i<CANT;i++){
+        lista[i].isEmpty = 0;
+    }
+
+    while (eleccion != 6){
+        printf("------------Administracion de empleados------------\n");
+        printf("1- Ingrese 1 para dar de alta a un empleado\n");
+        printf("2- Ingrese 2 para dar de baja a un empleado\n");
+        printf("3- Ingrese 3 para modificar los datos de un empleado\n");
+        printf("4- Ingrese 4 para ordenarlos alfabeticamente\n");
+        printf("5- Ingrese 5 para imprimir\n");
+        printf("6- Ingrese 6 para salir del programa\n\n");
+        getInt(&eleccion);
+        system("cls");
         switch (eleccion){
-            case 1:
-                if (bandera=espacioLibre()==-1){
-                    printf("No se encontro un espacio vacio");
+        case 1:
+            indiceLugarLibre = initEmployees(lista,CANT);
+            if(indiceLugarLibre == -1){
+                printf("No quedan lugares disponibles.\n");
+                break;
+            }
+            printf("\nAlta de empleado");
+            printf("\n\nIngrese el nombre: ");
+            getSringLetras(auxiliarNombre);
+            printf("\nIngrese el apellido: ");
+            getSringLetras(auxiliarApellido);
+            printf("\nIngrese el sector: ");
+            getInt(&auxSector);
+            printf("\nIngrese el salario: ");
+            getFloat(&auxSalario);
+            auxId = indiceLugarLibre;
+            addEmployee(lista,CANT,auxId,auxiliarNombre,auxiliarApellido,auxSalario,auxSector);
+            system("cls");
+            break;
+            case 2:
+                printf("Ingrese el id del empleado al que desea borrar del registro");
+                getInt(&auxId);
+                empleadoIdEncontrado = findEmployeeById(lista,CANT,auxId);
+                lista[empleadoIdEncontrado].isEmpty = 0;
+                break;
+            case 3:
+                printf("Ingrese el id del empleado al que desea modificar el registro");
+                getInt(&auxId);
+                empleadoIdEncontrado = findEmployeeById(lista,CANT,auxId);
+                if (empleadoIdEncontrado != -1){
+                    printf("\n\nIngrese el nombre: ");
+                    getSringLetras(auxiliarNombre);
+                    printf("\nIngrese el apellido: ");
+                    getSringLetras(auxiliarApellido);
+                    printf("\nIngrese el sector: ");
+                    getInt(&auxSector);
+                    printf("\nIngrese el salario: ");
+                    getFloat(&auxSalario);
+                    strcpy(lista[empleadoIdEncontrado].name,auxiliarNombre);
+                    strcpy(lista[empleadoIdEncontrado].lastName,auxiliarApellido);
+                    lista[empleadoIdEncontrado].sector = auxSector;
+                    lista[empleadoIdEncontrado].salary = auxSalario;
                 }
-                else if(bandera == 0){
-                    printf("Ingrese el nombre: ");
-                    scanf("%s",per[bandera].nombre);
-                    printf("Ingrese el apellido: ");
-                    scanf("%s",per[bandera].apellido);
-                    printf("Ingrese el dni: ");
-                    scanf("%s",per[bandera].dni);
-                    printf("Ingrese el dia de nacimiento: ");
-                    scanf("%d",&per[bandera].dia);
-                    printf("Ingrese el dia de mes: ");
-                    scanf("%d",&per[bandera].mes);
-                    printf("Ingrese el dia de anio: ");
-                    scanf("%d",&per[bandera].anio);
+                else {
+                    printf("El id buscado no se encuentra en el registro");
+                }
+            break;
+            case 4:
+                for (i=0;CANT;i++){
+                    if (lista[i].isEmpty!=0){
+                    for (j=0;CANT;j++){
+                        if (lista[j+1].isEmpty!=0)
+                            if (strcmp(lista[i].lastName,lista[j+1].lastName)>=1){
+                                strcpy(auxiliarChar,lista[i].lastName);
+                                strcpy(lista[i].lastName,lista[j+1].lastName);
+                                strcpy(lista[j+1].lastName,auxiliarChar);
+                            }
+                        }
+                    }
                 }
                 break;
+            case 5:
+                for (i=0;i<CANT;i++){
+                    if (lista[i].isEmpty == 1){
+                        printf("\nNombre: %s",lista[i].name);
+                        printf("\nApellido: %s",lista[i].lastName);
+                        printf("\nSector: %d",lista[i].sector);
+                        printf("\nSalario: %f",lista[i].salary);
+                        printf("\nId: %d\n\n",lista[i].id);
+                    }
+                }
+                break;
+            case 6:
+            printf("Ah salido del programa\n");
+            break;
+
         }
-    }while(salida != 1);
+    }
     return 0;
 }
